aac1376e8a63146967af852a61f744bbfd4c9278
R sai rajesh <rudrravarjula1234@gmail.com>
2018-04-16 18:55:31 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,178 @@
+
+<!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+<head><title>Leadership Foundation</title>
+	<meta name="format-detection" content="telephone=no">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="utf-8">
+	<link rel="icon" href="Web_Resources_Logos/favicon-16x16.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato:300,400,700,900,400italic">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/loader.css">
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<!--[if lt IE 10]>
+	<div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;">
+		<a href="https://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a>
+	</div>
+	
+	<script src="js/html5shiv.min.js"></script><![endif]-->
+	<style>
+		.pre-footer-default a{
+			color:black;
+		}
+		.thumbnail-modern a{
+			color: white;
+		}
+	</style>
+</head>
+	
+<body>
+	
+	
+	<section class="pre-footer-default novi-background bg-cover text-center text-sm-left">
+		<div class="shell">
+			<div class="range range-sm-center spacing-55">
+				<div class="cell-sm-12 cell-md-4">
+					<div >
+						<a href="index.html"><img src="Web_Resources_Logos/Leadership_Web.png"  alt="" width="190" height="55"></a></div>
+					<p>Webline Project is a perfect solution for everyone who wants to establish a stunningly looking website with
+						lots of useful functions and without extra coding.</p>
+						<div class="group-sm group-middle" ><span class="big text-primary">Social media</span>
+							<ul class="inline-list-xxs">
+								<li>
+									<a class="icon icon-xxs icon-circle icon-trout-outline icon-effect-1 fa fa-youtube novi-icon" href="https://"></a>
+								</li>
+								<li>
+									<a class="icon icon-xxs icon-circle icon-trout-outline icon-effect-1 fa fa-facebook novi-icon" href="https://www.facebook.com/leadershipfoundationofficial"></a>
+								</li>
+								<li>
+									<a class="icon icon-xxs icon-circle icon-trout-outline icon-effect-1 fa fa-twitter novi-icon" href="https://twitter.com/leadershipls"></a>
+								</li>
+								<li>
+									<a class="icon icon-xxs icon-circle icon-trout-outline icon-effect-1 fa fa-google-plus novi-icon" href="#"></a>
+								</li>
+							</ul>
+						</div>
+						<br>
+				
+				</div>
+				<div class="cell-sm-0 cell-md-1">
+				</div>
+				<div class="cell-sm-4 cell-md-2">
+					
+						<div class="brand-sm"><h4>Programs</h4></div>
+						<ul>
+							<li>
+								<a   href="">Parinama</a>
+							</li>
+							<li>
+								<a    href="">Clash Of Kingdoms</a>
+							</li>
+							<li>
+								<a   href="">CAP</a>
+							</li>
+							<li>
+									<a   href="">Unlimited</a>
+							</li>
+							<li>
+									<a   href="">Vyuha</a>
+							</li>
+							<li>
+									<a   href="">Prarambh</a>
+							</li>
+							<li>
+									<a   href="">Panchatantra</a>
+							</li>
+							<li>
+									<a   href="">Abhiyaan</a>
+							</li>
+							<li>
+									<a   href="">SMMP</a>
+							</li>
+						</ul>	
+					
+				</div>
+				<div class="cell-sm-4 cell-md-2">
+                    <div class="brand-sm"><h4>Our Works</h4></div>
+					
+					<ul>
+						<li>
+							<a href="#"  >Student Activity Center</a>
+						</li>
+						<li>
+							<a href="#"  >Techmology Incubation Hub</a>
+						</li>
+						<li>
+							<a   href="https://www.smartvillagerevolution.in">Smart Village Revolution</a>
+							
+						</li>
+						<li>
+							<a   href="">In Schools</a>
+						</li>
+					</ul>
+				</div>
+				<div class="cell-sm-4 cell-md-3">
+					<h4>About Leadership</h4>
+					<div>
+						<h3>	</h3>
+					</div>
+					<ul>
+						<li>
+							<a   href="">About Us</a>
+						</li>
+						<li>
+							<a   href="">Careers</a>
+						</li>
+						<li>
+							<a   href="">Success Stories</a>
+						</li>
+						<li>
+							<a   href="">Our StartUps</a>
+						</li>
+						<li>
+							<a   href="">Shop</a>
+						</li>
+						<li>
+							<a   href="">Jobs & Interships</a>
+						</li>
+						<li>
+							<a   href="">Press</a>
+						</li>
+						<li>
+							<a   href="">Contact</a>
+						</li>
+					</ul>
+
+				</div>
+			</div>
+		</div>
+
+	</section>
+	<footer class="page-footer-default novi-background bg-cover text-center">
+		<div class="shell">
+			<div class="range spacing-30 range-fix">
+				<div class="cell-md-8 cell-lg-8 text-md-left">
+					<ul class="list-nav">
+						<li class="active"><a href="index.html">Home</a></li>
+						<li><a href="#about-us">About us</a></li>
+						<li><a href="#services">Clubs</a></li>
+						<li><a href="#clients">Affiliations</a></li>
+						<li><a href="#testimonials">Testimonials</a></li>
+						<li><a href="login.html">Login</a></li>
+					</ul>
+				</div>
+				<div class="cell-md-4 cell-lg-4 text-md-right"><p class="rights">Leadership Foundation&nbsp;©&nbsp;
+					<span id="copyright-year"></span>.&nbsp; <br class="veil-sm"></p></div>
+			</div>
+		</div>
+	</footer>
+
+<!-- coded by Himic -->
+<div class="snackbars" id="form-output-global"></div>
+
+
+<script src="js/core.min.js"></script>
+<script src="js/script.js"></script>
+</html>
---
+++ header.php
@@ -0,0 +1,78 @@
+
+<!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+<head><title>Leadership Foundation</title>
+	<meta name="format-detection" content="telephone=no">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="utf-8">
+	<link rel="icon" href="Web_Resources_Logos/favicon-16x16.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato:300,400,700,900,400italic">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/loader.css">
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<!--[if lt IE 10]>
+	<div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;">
+		<a href="https://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a>
+	</div>
+	
+	<script src="js/html5shiv.min.js"></script><![endif]-->
+	
+</head>
+	
+<body>
+	
+	<header class="page-header" id="home" data-type="anchor">
+		<div class="rd-navbar-wrap">
+			<nav class="rd-navbar rd-navbar-transparent" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-sm-device-layout="rd-navbar-fixed" data-md-layout="rd-navbar-static" data-md-device-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-static" data-lg-layout="rd-navbar-static" data-body-class="rd-navbar-absolute-linked swiper-jumbotron-mod" data-stick-up-clone="false" data-md-stick-up-offset="72px" data-lg-stick-up-offset="72px" data-stick-up="true" data-sm-stick-up="true" data-md-stick-up="true" data-lg-stick-up="true">
+				<div class="rd-navbar-top-panel novi-background bg-cover">
+					<div class="rd-navbar-top-panel-toggle" data-rd-navbar-toggle=".rd-navbar-top-panel"><span></span></div>
+					<div class="rd-navbar-top-panel-content">
+						<div class="rd-navbar-top-panel-content-inner">
+							<ul class="inline-list-xs">
+								<li><a class="icon icon-xs icon-white fa fa-facebook novi-icon" href="https://www.facebook.com/leadershipfoundationofficial"></a></li>
+								<li><a class="icon icon-xs icon-white fa fa-twitter novi-icon" href="https://twitter.com/leadershipls"></a></li>
+								<li><a class="icon icon-xs icon-white fa fa-google-plus novi-icon" href="#"></a></li>
+								<li><a class="icon icon-xs icon-white fa fa-youtube novi-icon" href="#"></a></li>
+							</ul>
+							<address class="contact-info"></address>
+						</div>
+						<div class="object-inline">
+                            <span class="icon icon-sm icon-white fa fa-envelope novi-icon"></span><a class="link link-sm link-white" href="tel:#">contact@leadershipfoundation.info</a>
+                            <span class="icon icon-sm icon-white fa fa-user novi-icon"><a class="link link-sm link-white" href="#" style="margin-left:6px;">Login</a></span>
+                        </div>
+					</div>
+				</div>
+				<div class="rd-navbar-inner">
+					<div class="rd-navbar-panel">
+						<button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+						<div class="rd-navbar-brand">
+							<a class="brand-name" href="index.html"><img src="Web_Resources_Logos/Leadership_Web.png" alt="" width="191" height="55	"></a>
+						</div>
+					</div>
+					<div class="rd-navbar-nav-wrap">
+						<div class="rd-navbar-nav-wrap-bg"></div>
+						<ul class="rd-navbar-nav">
+							<li class="active"><a href="#home">Home</a></li>
+							<li><a href="#about-us">About Us</a></li>
+							<li><a href="#services">Clubs</a></li>
+							<li><a href="#clients">Affiliations</a></li>
+							<li><a href="#testimonials">Testimonials</a></li>
+							<li><a href="#events">Events</a></li>
+							
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+		
+		
+	</header>
+	
+
+<!-- coded by Himic -->
+
+<script src="js/core.min.js"></script>
+<script src="js/script.js"></script>
+</html>
